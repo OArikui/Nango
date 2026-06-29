33270f227be328913f0bed764fc81d2f400b7f03
OArikui <Arikui57@outlook.jp>
2026-06-29 20:24:14 +0900

    change base

--- test_or_future/DeepDiff_Try.py
+++ test_or_future/DeepDiff_Try.py
@@ -2,29 +2,34 @@ from deepdiff import DeepDiff
 from pprint import pprint
 
 dict_base = {
-    "dict_a": {"dictK_a": "dictV_a"},
+    "dict_a": {
+        "dictK0_a": "dictV0_a",
+        "dictK1_a": "dictV1_a",
+        "dictK2_a": "dictV2_a",
+    },
     "arr_a": ["el0_a", "el1_a"],
 }
 
 old = {
-    "dict_a": {"dictK_o": "dictV_a"},
+    "dict_a": {
+        "dictK0_o": "dictV0_a",
+        "dictK1_a": "dictV1_a",
+        "dictK2_a": "dictV2_a",
+    },
     "arr_a": ["el0_a", "el1_a"],
 }
 
 new = {
-    "dict_a": {"dictK_n": "dictV_a"},
+    "dict_a": {
+        "dictK0_n": "dictV0_a",
+        "dictK1_a": "dictV1_a",
+        "dictK2_a": "dictV2_a",
+    },
     "arr_a": ["el0_a", "el1_a"],
 }
 
 pprint(DeepDiff(old, new, ignore_order=False))
 """
 result
-{
-    "values_changed": {
-        "root['dict_a']": {
-            "new_value": {"dictK_n": "dictV_a"},
-            "old_value": {"dictK_o": "dictV_a"},
-        }
-    }
-}
+None
 """
