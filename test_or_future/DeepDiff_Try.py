ac5ff61186d13d3137cee5fcce49a9bc0bd5eb02
OArikui <Arikui57@outlook.jp>
2026-06-29 20:09:54 +0900

    root=dict　D0のkeyを変える。 仮説:itemのadd,removeはkey依存? (resultはfile参照) D0=root直下

--- test_or_future/DeepDiff_Try.py
+++ test_or_future/DeepDiff_Try.py
@@ -7,13 +7,30 @@ dict_base = {
 }
 
 old = {
-    "dict_a": {"dictK_a": "dictV_a"},
-    "arr_a": ["el0_a", "el1_a"],
+    "dict_o": {"dictK_a": "dictV_a"},
+    "arr_o": ["el0_a", "el1_a"],
 }
 
 new = {
-    "dict_a": {"dictK_a": "dictV_a"},
-    "arr_a": ["el0_a", "el1_a"],
+    "dict_n": {"dictK_a": "dictV_a"},
+    "arr_n": ["el0_a", "el1_a"],
 }
 
 pprint(DeepDiff(old, new, ignore_order=False))
+"""
+result
+{
+    "values_changed": {
+        "root": {
+            "new_value": {
+                "arr_n": ["el0_a", "el1_a"],
+                "dict_n": {"dictK_a": "dictV_a"},
+            },
+            "old_value": {
+                "arr_o": ["el0_a", "el1_a"],
+                "dict_o": {"dictK_a": "dictV_a"},
+            },
+        }
+    }
+}
+"""
