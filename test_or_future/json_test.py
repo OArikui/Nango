5b7426a44ed79db7aaca76eacea217efd5cb1361
OArikui <Arikui57@outlook.jp>
2026-06-30 19:59:10 +0900

    feat:重複項目の抽出関数を作製

--- test_or_future/json_test.py
+++ test_or_future/json_test.py
@@ -1,6 +1,12 @@
 import json
+import re
 from jsonschema import Draft202012Validator
 from jsonschema.exceptions import ValidationError
+from deepdiff import DeepDiff, extract
+
+"""Json.v05.00~に対応してます。"""
+
+# 今は複数ファイルの同時importは想定していません。
 
 
 def JSON_load_word(subject, schema):
@@ -13,6 +19,34 @@ def JSON_load_word(subject, schema):
     except ValidationError as e:
         print(e.message)
     print("END")
+
+def redundancy_JSON(wordsA, wordsB):
+    """
+    FMTの審査を通り、クレンジング済みのものを渡してください
+    二つのwords_listを受け取って,`word`の値を基準に重複を解決します。
+    wordsA,BはJSON_FMTそのままの'row_db'についてのrow_db['words']を指します。
+    """
+    setA = set(wordsA.keys())
+    setB = set(wordsB.keys())
+
+    intersection = setA.intersection(setB)
+
+    if not intersection:
+        print("no intersection")
+
+    else:
+        keys_DeepDiff = [
+            "dictionary_item_added",
+            "iterable_item_added",
+            "values_changed",
+            "dictionary_item_removed",
+            "iterable_item_removed",
+            "type_changed",
+        ]
+
+        for isect_word in intersection:
+            objA = setA[isect_word]
+            diffAB = DeepDiff(wordsA[isect_word], wordsB[isect_word], ignore_order=True)
 
 
 if __name__ == "__main__":
